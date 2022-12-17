8717e90111eb0553cd9b39eda3f9d9961c9d0091
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-12-17 17:02:22 -0600

    Added autobuild script

---
+++ build_and_push.sh
@@ -0,0 +1,13 @@
+docker build -f Dockerfile.mty . -t registry.internal.hisao.org/prod/multas-mty
+docker build -f Dockerfile.spgg . -t registry.internal.hisao.org/prod/multas-spgg
+docker build -f Dockerfile.candidates . -t registry.internal.hisao.org/prod/candidate-plates
+docker build -f Dockerfile.datacleaning . -t registry.internal.hisao.org/prod/plate-data-cleaning
+
+docker push registry.internal.hisao.org/prod/multas-mty
+docker push registry.internal.hisao.org/prod/multas-spgg
+docker push registry.internal.hisao.org/prod/candidate-plates
+docker push registry.internal.hisao.org/prod/plate-data-cleaning
+
+echo ""
+echo ""
+echo "Built and Pushed 3 Docker Images Successfully"
