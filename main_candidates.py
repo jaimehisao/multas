0a978d71bdf33f14d9fdc551cb8f08104bfff430
Hisao <32875647+jaimehisao@users.noreply.github.com>
2022-12-17 17:51:14 -0600

    Splitted mains into multiple files

---
+++ main_candidates.py
@@ -0,0 +1,10 @@
+from plate_existance_predictor import retrieve_plate_queue_and_check_validity_of_neighbhors
+
+
+def main():
+    while True:
+        retrieve_plate_queue_and_check_validity_of_neighbhors()
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_mty.py
@@ -0,0 +1,10 @@
+from plates import query_candidate_plates
+
+
+def main():
+    while True:
+        query_candidate_plates()
+
+
+if __name__ == "__main__":
+    main()
