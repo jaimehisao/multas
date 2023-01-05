18538da54ea21f2c6c4771e8d60d2b9e07963581
Hisao <32875647+jaimehisao@users.noreply.github.com>
2023-01-05 09:21:12 -0600

    Added constants file

---
+++ constants.py
@@ -0,0 +1,7 @@
+
+# Endpoints of ASP backends from where the tickets will be retrieved.
+SAN_PEDRO_ASP_ENDPOINT = "https://aplicativos.sanpedro.gob.mx/esanpedro/multas/estado_cuenta.asp"
+MONTERREY_ASP_ENDPOINT = ""
+
+# Sets the amount of plates that will be considered candidates after a positive match.
+CANDIDATE_PLATES_AFTER_POSITIVE_MATCH = 10 + 1
