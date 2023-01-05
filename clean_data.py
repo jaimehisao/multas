cae403c3a41191644ab8c5c74a8949e92a108e8a
Hisao <32875647+jaimehisao@users.noreply.github.com>
2023-01-05 09:26:19 -0600

    Simplified the way that data is cleaned using SQL querieS

--- clean_data.py
+++ clean_data.py
@@ -1,33 +1,12 @@
-from database import cursor, conn
+from database import (
+    clean_parking_meter_tickets,
+    clean_prohibited_parking_space_tickets,
+    other_cleanups,
+)
 
 
-def clean_parking_meter_tickets():
-    cursor.execute(
-        "UPDATE tickets "
-        "SET ticket_type = 'MULTA DE PARQUIMETROS' "
-        "WHERE ticket_type = 'NO PAGAR CUOTA PARQUIMETRO'"
-    )
-    cursor.execute(
-        "UPDATE tickets "
-        "SET ticket_type = 'MULTA DE PARQUIMETROS' "
-        "WHERE ticket_type = 'No paga cuota parquimetro'"
-    )
-    conn.commit()
-
-
-
-
-### Switching to using a direct update statement instead of SELECT then UPDATE
-"""
-def clean_ticket_names():
-    # Change ticket names to be more readable and consistent
-    cursor.execute("SELECT * FROM tickets")
-    tickets = cursor.fetchall()
-    for ticket in tickets:
-        if ticket[4] == "No paga cuota parquimetro":
-            cursor.execute(
-                "UPDATE tickets SET ticket_type = %s WHERE ticket_number = %s", ## altough this might dupe it in some mty tickets, probs should check type first
-                ("MULTA DE PARQUIMETROS", ticket[1]),
-            )
-            conn.commit()
-"""
+# Could probably clean up into one single command and/or abstract from DB class.
+clean_parking_meter_tickets()
+clean_prohibited_parking_space_tickets()
+other_cleanups()
+print("Data cleaned!")
--- database.py
+++ database.py
@@ -1,10 +1,9 @@
 import psycopg2
 from dotenv import load_dotenv
 import os
-
+import datetime as dt
 
 load_dotenv()
-
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
@@ -19,39 +18,339 @@ conn = psycopg2.connect(
 
 cursor = conn.cursor()
 
+###################################
+############ CLEANING ############
+###################################
+def clean_parking_meter_tickets():
+
+    # Parking Meter
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'MULTA DE PARQUIMETROS' "
+        "WHERE ticket_type = 'NO PAGAR CUOTA PARQUIMETRO'"
+    )
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'MULTA DE PARQUIMETROS' "
+        "WHERE ticket_type = 'No paga cuota parquimetro'"
+    )
+    conn.commit()
+
+
+def clean_prohibited_parking_space_tickets():
+    # Prohibited Parking Spaces
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO   , ART. 72 FRACC. III, IV, V, VI, VII, VIII, IX, X, XI, XII, XIII, XV, XVI, XVII, XX, X'"
+    )
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'ESTAC EN LUGAR PROHIBIDO'"
+    )
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'Est. En lugar Prohibido'"
+    )
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'ESTAC. LUGAR PROHIBIDO'"
+    )
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO   , ART. 72'"
+    )
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'ESTACIONARSE EN LUGAR PROHIBIDO   , ART. 72'"
+    )
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'CAMBIAR CARRIL SIN PRECAUCION' "
+        "WHERE ticket_type = 'cambiar carr s/ prec'"
+    )
+
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'REBASAR EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'REBASAR EN LUGAR PROHIBID'"
+    )
+
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'SER RESPONSABLE EN HECHO DE TRÁNSITO' "
+        "WHERE ticket_type = 'SER RESPONSABLE EN HECHO DE TRÁNSITO , ART. 137'"
+    )
+
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'CIRCULAR SIN LICENCIA O VENCIDA' "
+        "WHERE ticket_type = 'circ. sin licencia o vencida'"
+    )
+
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'VOLTEAR A LA IZQUIERDA EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'VOLTEAR A LA IZQ EN LUGAR PROH'"
+    )
+
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'TRANSPORTAR PERSONAS EN LUGAR PROHIBIDO' "
+        "WHERE ticket_type = 'TRANS.PERS.EN LUGAR PROH.'"
+    )
+
+    cursor.execute(
+        "UPDATE tickets "
+        "SET ticket_type = 'SUJETAR APARATOS DE COMUNICACION' "
+        "WHERE ticket_type = 'SUJETAR APARATOS DE COMUNICACIÓN O APARATOS ELECTRÓNICOS , ART. 51, FRACC. II'"
+    )
+
+    conn.commit()
+
+
+def other_cleanups():
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'MANEJAR CON LICENCIA VENCIDA' "
+        "WHERE ticket_type = 'MANEJAR C/LIC.VENCIDA'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'CIRCULAR ZIGZAGUEANDO' "
+        "WHERE ticket_type = 'CIRC. ZIGZAGUEANDO'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'INICIAR MARCHA SIN PRECAUCIÓN' "
+        "WHERE ticket_type = 'INICIAR MARCHA SIN PRECAUCIÓN , ART. 49, FRACC. XV'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'MANEJAR LICENCIA NO ACORDE AL VEHICULO' "
+        "WHERE ticket_type = 'MANEJAR LICEN.NO ACORDE AL VEH'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'NO CONTAR CON CINTURON DE SEGURIDAD' "
+        "WHERE ticket_type = 'NO CONTAR CON CINTURON DE SEG.'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'INTERVENIR EN ASUNTOS DE TRANSITO' "
+        "WHERE ticket_type = 'INTERVENIR EN ASUNTOS DE TRANS'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'ESTACIONARSE EN LUGAR EXCLUSIVO PARA DISCAPACITADOS' "
+        "WHERE ticket_type = 'ESTACIONARSE EN LUGAR EXCLUSIVO PARA DISCAPACITADOS , ART. 72, FRACC. XVIII'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'EXCESO DE VELOCIDAD EN ZONA ESCOLAR' "
+        "WHERE ticket_type = 'EXCESO DE VELOCIDAD EN ZONA ESCOLAR , ART. 52, FRACC. I'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'EXCESO DE VELOCIDAD' "
+        "WHERE ticket_type = 'exceso de velocidad'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'TRASLADO DE LESIONADO CRUZ ROJA' "
+        "WHERE ticket_type = 'TRASLADO DE LESIONADO C. ROJA'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'EXCESO DE VELOCIDAD EN ZONA URBANA' "
+        "WHERE ticket_type = 'EXCESO DE VELOCIDAD Z. URBANA'"
+    )
+    cursor.execute(
+        "UPDATE tickets SET ticket_type = 'EXCESO DE VELOCIDAD EN ZONA ESCOLAR' "
+        "WHERE ticket_type = 'Exceso vel. Zona Escolar'"
+    )
+    conn.commit()
+
+
+####### END OF CLEANING SECTION #######
+
+
+############ UPDATES ############
+def update_last_retrieved_to_right_now(plate: str) -> None:
+    """
+    Updates the last retrieved date
+    :param plate: Plate to update
+    :param date: Date to update to
+    :return: None
+    """
+    ts = dt.datetime.now()
+    cursor.execute(
+        "UPDATE plates SET last_retrieved = %s WHERE plate = %s",
+        (
+            ts,
+            plate,
+        ),
+    )
+    conn.commit()
+
+
+def update_last_retrieved_to_right_now_mty(plate: str) -> None:
+    """
+    Updates the last retrieved date
+    :param plate: Plate to update
+    :param date: Date to update to
+    :return: None
+    """
+    ts = dt.datetime.now()
+    cursor.execute(
+        "UPDATE plates SET last_retrieved_mty = %s WHERE plate = %s",
+        (
+            ts,
+            plate,
+        ),
+    )
+    conn.commit()
+
+
+def update_last_retrieved_to_right_now_spgg(plate: str) -> None:
+    """
+    Updates the last retrieved date
+    :param plate: Plate to update
+    :param date: Date to update to
+    :return: None
+    """
+    ts = dt.datetime.now()
+    cursor.execute(
+        "UPDATE plates SET last_retrieved_spgg = %s WHERE plate = %s",
+        (
+            ts,
+            plate,
+        ),
+    )
+    conn.commit()
+
+
+############ PLATE QUEUE & CANDIDATES ############
+def get_plate_queue() -> list:
+    """
+    Gets the plate queue from the database
+    :return: List of plates in the queue
+    """
+    cursor.execute("SELECT plate FROM plate_queue ORDER BY plate")
+    return cursor.fetchall()
+
+
+def remove_candidate(plate):
+    cursor.execute("DELETE FROM plate_queue WHERE plate = %s", (plate,))
+    conn.commit()
+
+
+def mark_plate_as_candidate(plate):
+    cursor.execute("SELECT found FROM plates WHERE plate = %s", (plate,))
+    has_been_found = cursor.fetchall()
+    if len(has_been_found) > 0:
+        # If a plate has been found (len!=0), it is not a candidate, because it has already been searched.
+        print(
+            "Plate has already been found, not marking as candidate and deleting from queue."
+        )
+        cursor.execute(
+            "DELETE FROM plate_queue WHERE plate = %s", (plate,)
+        )  # might break code if i run the script twice
+        conn.commit()
+    else:
+        cursor.execute(
+            "UPDATE plate_queue SET candidate = true WHERE plate = %s", (plate,)
+        )
+        cursor.execute(
+            "UPDATE plate_queue SET last_checked_mty = %s WHERE plate = %s",
+            (dt.datetime.strptime("2020/01/01", "%Y/%m/%d"), plate),
+        )
+        cursor.execute(
+            "UPDATE plate_queue SET last_checked_spgg = %s WHERE plate = %s",
+            (dt.datetime.strptime("2020/01/01", "%Y/%m/%d"), plate),
+        )
+        conn.commit()
+
+
+def mark_plates_as_candidates(plate):
+    for plate in plate:
+        cursor.execute("UPDATE plates SET candidate = true WHERE plate = %s", (plate,))
+    conn.commit()
+
+
+def get_found_plates_in_order():
+    cursor.execute(
+        "SELECT plate FROM plates WHERE found = true or found_mty = true ORDER BY plate"
+    )
+    return cursor.fetchall()
+
+
+######### temporary #########
+def get_candidates_mty():
+    cursor.execute(
+        "SELECT plate, last_checked_mty "
+        "FROM plate_queue "
+        "WHERE candidate = true "
+        "ORDER BY last_checked_mty"
+    )
+    return cursor.fetchall()
+
+
+def get_spgg_candidates():
+    cursor.execute(
+        "SELECT plate, last_checked_spgg "
+        "FROM plate_queue "
+        "WHERE candidate = true "
+        "ORDER BY last_checked_spgg"
+    )
+    return cursor.fetchall()
+
+
+######### end temporary #########
+
+
+def mark_candidate_last_checked_date_mty(plate):
+    cursor.execute(
+        "UPDATE plate_queue SET last_checked_mty = %s WHERE plate = %s",
+        (dt.datetime.now(), plate),
+    )
+    conn.commit()
+
+
+def mark_candidate_last_checked_date_spgg(plate):
+    cursor.execute(
+        "UPDATE plate_queue SET last_checked_spgg = %s WHERE plate = %s",
+        (dt.datetime.now(), plate),
+    )
+    conn.commit()
+
 
 def is_plate_found(plate):
     cursor.execute("SELECT found, found_mty FROM plates WHERE plate = %s", (plate,))
     return cursor.fetchall()
 
 
-def mark_plate_as_candidate(plate):
-    cursor.execute("UPDATE plate_queue SET candidate = true WHERE plate = %s", (plate,))
-    conn.commit()
-
-
-def get_plate_queue() -> list:
-    """
-    Gets the plate queue from the database
-    :return: List of plates in the queue
-    """
-    cursor.execute("SELECT plate FROM plate_queue")
-    return cursor.fetchall()
-
-
 def add_plates_to_db_queue(plates: []) -> None:
     """
-    Adds plates to the database queue
+    Adds plates to the database queue.
     :param plates: List of plates to add
     :return: None
     """
+    index = 0
     for plate in plates:
-        cursor.execute("SELECT * FROM plate_queue WHERE plate = %s", (plate,))
-        if len(cursor.fetchall()) == 0:
-            print("Adding plate to queue: " + plate)
-            cursor.execute("INSERT INTO plate_queue(plate) VALUES (%s)", (plate,))
-            conn.commit()
-
+        # cursor.execute("SELECT * FROM plate_queue WHERE plate = %s", (plate,))
+        if index % 1000 == 0:
+            print(
+                "Current progress: "
+                + str(index)
+                + "/"
+                + str(len(plates))
+                + " plates ("
+                + str(round(index / len(plates) * 100, 2))
+                + "%)"
+            )
+            # conn.commit()
+        # if len(cursor.fetchall()) == 0:
+        cursor.execute(
+            "INSERT INTO plate_queue(plate) VALUES (%s) ON CONFLICT DO NOTHING",
+            (plate,),
+        )
+        index += 1
+    conn.commit()
 
 
 def get_stored_plates() -> list:
@@ -76,11 +375,16 @@ def get_found_mty_plates():
 def add_ticket_mty(plate, tickets):
     for tix in tickets:
         cursor.execute(
-            "SELECT * FROM tickets WHERE ticket_number = %s", (tix["boleta"],)
+            "SELECT * FROM tickets WHERE ticket_number = %s and ticket_type = %s",
+            (
+                tix["boleta"],
+                tix["infraccion"],
+            ),
         )
         if len(cursor.fetchall()) == 0:
             cursor.execute(
-                "INSERT INTO tickets(municipality, ticket_number, date, ticket_type, crossing, discount, total, plate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
+                "INSERT INTO tickets(municipality, ticket_number, date, ticket_type, crossing, discount, total, plate) "
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",
                 (
                     tix["municipio"],
                     tix["boleta"],
@@ -96,7 +400,14 @@ def add_ticket_mty(plate, tickets):
 
 
 def get_found_plates():
-    cursor.execute("SELECT plate FROM plates WHERE found = true")
+    cursor.execute("SELECT plate FROM plates WHERE found = true or found_mty = true")
+    return cursor.fetchall()
+
+
+def get_found_plates_and_last_query_time():
+    cursor.execute(
+        "SELECT plate, last_retrieved FROM plates WHERE found = true or found_mty = true"
+    )
     return cursor.fetchall()
 
 
@@ -124,19 +435,22 @@ def mark_plate_as_found(plate: str) -> None:
     conn.commit()
 
 
-def add_new_tickets(plate: str, tickets: list) -> None:
+def add_new_spgg_tickets(plate: str, tickets: list) -> None:
     """
     Adds new tickets to the database
     :param tickets: List of tickets to add
     :return: None
     """
+    # should check municipality
     for multa in tickets:
         cursor.execute(
             "SELECT * FROM tickets WHERE ticket_number = %s", (multa["boleta"],)
         )
         if len(cursor.fetchall()) == 0:
             cursor.execute(
-                "INSERT INTO tickets(municipality, ticket_number, date, ticket_type, crossing, value, discount, total, payment_date, reciept2, payed, balance, plate) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
+                "INSERT INTO tickets(municipality, ticket_number, date, ticket_type, crossing, value, discount, total, "
+                "payment_date, reciept2, payed, balance, plate) "
+                "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
                 (
                     multa["municipio"],
                     multa["boleta"],
