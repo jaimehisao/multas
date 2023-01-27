0c6e04e2bb6c18aec6259fcd7a3b89c1e9521e5f
Hisao <32875647+jaimehisao@users.noreply.github.com>
2023-01-27 19:06:50 +0100

    updated build and push script

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,10 +1,14 @@
-docker build -f Dockerfile.mty . -t registry.internal.hisao.org/prod/multas-mty
-docker build -f Dockerfile.spgg . -t registry.internal.hisao.org/prod/multas-spgg
+docker build -f Dockerfile.candidates_mty . -t registry.internal.hisao.org/prod/multas-mty-candidates
+docker build -f Dockerfile.candidates_spgg . -t registry.internal.hisao.org/prod/multas-spgg-candidates
+docker build -f Dockerfile.updater_mty . -t registry.internal.hisao.org/prod/multas-mty-updater
+docker build -f Dockerfile.updater_spgg . -t registry.internal.hisao.org/prod/multas-spgg-updater
 docker build -f Dockerfile.candidates . -t registry.internal.hisao.org/prod/candidate-plates
 docker build -f Dockerfile.datacleaning . -t registry.internal.hisao.org/prod/plate-data-cleaning
 
-docker push registry.internal.hisao.org/prod/multas-mty
-docker push registry.internal.hisao.org/prod/multas-spgg
+docker push registry.internal.hisao.org/prod/multas-mty-candidates
+docker push registry.internal.hisao.org/prod/multas-spgg-candidates
+docker push registry.internal.hisao.org/prod/multas-mty-updater
+docker push registry.internal.hisao.org/prod/multas-spgg-updater
 docker push registry.internal.hisao.org/prod/candidate-plates
 docker push registry.internal.hisao.org/prod/plate-data-cleaning
 
