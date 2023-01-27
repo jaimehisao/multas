eed2d82174d1920c02a717cdd8c16cfc9c8eb5e5
Hisao <32875647+jaimehisao@users.noreply.github.com>
2023-01-27 19:06:34 +0100

    Added new mains for updating processes

---
+++ main_updater_mty.py
@@ -0,0 +1,10 @@
+from plates import update_mty_plates
+
+
+def main():
+    while True:
+        update_mty_plates()
+
+
+if __name__ == "__main__":
+    main()
---
+++ main_updater_spgg.py
@@ -0,0 +1,10 @@
+from plates import update_spgg_plates
+
+
+def main():
+    while True:
+        update_spgg_plates()
+
+
+if __name__ == "__main__":
+    main()
