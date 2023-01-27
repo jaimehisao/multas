dc15a4435c5f847aca49f897aa43e350c93b978e
Hisao <32875647+jaimehisao@users.noreply.github.com>
2023-01-27 18:20:47 +0100

    nit fix

--- constants.py
+++ constants.py
@@ -3,8 +3,8 @@
 SAN_PEDRO_ASP_ENDPOINT = "https://aplicativos.sanpedro.gob.mx/esanpedro/multas/estado_cuenta.asp"
 MONTERREY_ASP_ENDPOINT = "https://asp.monterrey.gob.mx/MultasTransito/"
 
-# Sets the amount of plates that will be considered candidates after a positive match.
-CANDIDATE_PLATES_AFTER_POSITIVE_MATCH = 20 + 1
+# Sets the amount of plates that will be considered candidates after a positive match. (Modify second value)
+CANDIDATE_PLATES_AFTER_POSITIVE_MATCH = 1 + 20
 
 # Sets the minimum time of days when a plate will be queried again, this to provide all plates an equal chance.
 QUERY_COOLDOWN_DAYS = 90
