01f2045d7f56e6b66df7a5cec574658fa8a9d0a9
Hisao <32875647+jaimehisao@users.noreply.github.com>
2023-01-30 12:51:09 +0100

    simplified the way that dockerimages are built

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,9 +1,12 @@
+docker build -f Dockerfile.base . -t registry.internal.hisao.org/prod/multas-base
+
 docker build -f Dockerfile.candidates_mty . -t registry.internal.hisao.org/prod/multas-mty-candidates
 docker build -f Dockerfile.candidates_spgg . -t registry.internal.hisao.org/prod/multas-spgg-candidates
 docker build -f Dockerfile.updater_mty . -t registry.internal.hisao.org/prod/multas-mty-updater
 docker build -f Dockerfile.updater_spgg . -t registry.internal.hisao.org/prod/multas-spgg-updater
 docker build -f Dockerfile.candidates . -t registry.internal.hisao.org/prod/candidate-plates
-docker build -f Dockerfile.datacleaning . -t registry.internal.hisao.org/prod/plate-data-cleaning
+
+docker tag registry.internal.hisao.org/prod/multas-base registry.internal.hisao.org/prod/plate-data-cleaning
 
 docker push registry.internal.hisao.org/prod/multas-mty-candidates
 docker push registry.internal.hisao.org/prod/multas-spgg-candidates
